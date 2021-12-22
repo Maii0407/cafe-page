5f5d25a63fa83d135650737c32ee91a2c4c7cfbd
Akmal Izuddin <izuddin0407@gmail.com>
2021-12-22 09:44:07 +0800

    completed the menu section. next is making so user can switch between sections

--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,14 @@ import eeveeLatteIcon from './menu-items/eevee-latte.png';
 import liligantTeaIcon from './menu-items/liligant-tea.png';
 import bunearyFrappeIcon from './menu-items/buneary-frappe.png';
 import pachirisuFloatIcon from './menu-items/pachirisu-float.png';
+import swabluIceIcon from './menu-items/swablu-shaved-ice.png';
+import vulpixSundaeIcon from './menu-items/vulpix-sundae.png';
+import pikachuFlanIcon from './menu-items/pikachu-flan.png';
+import ribombeeCakeIcon from './menu-items/ribombee-cake.png';
+import snorlaxMocoIcon from './menu-items/snorlax-moco.png';
+import pikachuCurryIcon from './menu-items/pikachu-curry.png';
+import yamperPastaIcon from './menu-items/yamper-pasta.png';
+import torchicOmuriceIcon from './menu-items/torchic-omurice.png';
 
 const Menu = (function(){
     const mainContent = document.querySelector('.main-content');
@@ -46,24 +54,35 @@ const Menu = (function(){
 
     _myDrinks.push(eeveeLatte,liligantTea,bunearyFrappe,pachirisuFloat);
 
-    function table(){
-        console.table(_myDrinks);
-    };
+    const snorlaxMoco = new _MenuItems(`Happy Snorlax Loco Moco `, snorlaxMocoIcon, `A satisfying loco moco that will put a smile on Snorlax's face. It even comes with a cup of soup!`);
+    const pikachuCurry = new _MenuItems(`Piquant Pikachu Curry`, pikachuCurryIcon, `A vegetable-loaded curry with a kick to it. Enjoy a big plate and fuel your energy with Pikachu's smile.`);
+    const yamperPasta = new _MenuItems(`Yummy Yamper Pasta`, yamperPastaIcon, `A pasta bolognese depicting a playful Yamper. This cute and yummy dish will surely delight your taste buds and heart!`);
+    const torchicOmurice = new _MenuItems('Torchic Rice over Omelet', torchicOmuriceIcon, `Rice served atop an omelet as fluffy as Torchic's feathers. It goes well with ketchup!`);
 
-    const _makeDrinks = function(){
-        const drinksContainer = document.createElement('div');
-        drinksContainer.classList.add('menuItem-container');
+    _myDishes.push(snorlaxMoco, pikachuCurry, yamperPasta, torchicOmurice);
 
-        menuContainer.appendChild(drinksContainer);
+    const swabluIce = new _MenuItems(`Swablu Shaved Ice`, swabluIceIcon,`Shaved ice shaped like a Swablu. Make sure you eat up before it melts!`);
+    const vulpixSundae = new _MenuItems(`Burnt-Caramel Vulpix Sundae`, vulpixSundaeIcon,`A sweet and bitter dessert that depicts the cuteness and beauty of Vulpix. It tastes as good as it looks!`);
+    const pikachuFlan = new _MenuItems(`Pikachu Fruit Flan`, pikachuFlanIcon,`A decadent fruit flan loaded with fruits. Pikachu's smile is too cute to share!`);
+    const ribombeeCake = new _MenuItems(`Ribombee Chocolate Cake`, ribombeeCakeIcon,`An elegant bittersweet chocolate cake decorated with gold leaf. The Pollen Puff-like chocolate truffle simply melts in your mouth.`);
 
-        for(let obj of _myDrinks){
-            const drinksCard = document.createElement('div');
-            drinksCard.classList.add('menuItem-card');
-            drinksContainer.appendChild(drinksCard);
+    _myDesserts.push(swabluIce, vulpixSundae, pikachuFlan, ribombeeCake);
+
+//below is function that makes the menu based on name of array entered
+    const _makeMenu = function(array){
+        const menuItemContainer = document.createElement('div');
+        menuItemContainer.classList.add('menuItem-container');
+
+        menuContainer.appendChild(menuItemContainer);
+
+        for(let obj of array){
+            const menuItemCard = document.createElement('div');
+            menuItemCard.classList.add('menuItem-card');
+            menuItemContainer.appendChild(menuItemCard);
 
             const textSection = document.createElement('div');
             textSection.classList.add('menuText-section');
-            drinksCard.appendChild(textSection);
+            menuItemCard.appendChild(textSection);
 
             const h1 = document.createElement('h1');
             h1.textContent = obj.name;
@@ -75,17 +94,18 @@ const Menu = (function(){
 
             const img = document.createElement('img');
             img.src = obj.image;
-            drinksCard.appendChild(img);
+            menuItemCard.appendChild(img);
         }
     };
 
     const makeMenu = function(){
         Default.makeDefault();
-        table();
         _makeHeader('DRINKS');
-        _makeDrinks();
+        _makeMenu(_myDrinks);
         _makeHeader('DISHES');
+        _makeMenu(_myDishes);
         _makeHeader('DESSERT');
+        _makeMenu(_myDesserts);
     };
 
     return{
